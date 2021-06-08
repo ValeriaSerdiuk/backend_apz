dec0723bfa8c336e5a009c09a1f6e242f8dc819e
ValeriaSerdiuk <valeriia.serdiuk@nure.ua>
2021-06-09 02:18:04 +0300

    new

--- backendapz/accounts/views.py
+++ backendapz/accounts/views.py
@@ -7,7 +7,4 @@ from rest_framework import status
 from rest_framework.response import Response
 
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def restricted(request, *args, **kwargs):
-    return Response(data="Only for Logged in User", status=status.HTTP_200_OK)
+
